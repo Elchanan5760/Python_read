b7f22172133123bf786d192cd70a9a2b7079905e
Elchanan5760 <emstone@gmail.com>
2025-06-24 12:39:33 +0300

    create new text

---
+++ create_txt.py
@@ -0,0 +1,12 @@
+
+try:
+    with open(".venv/my_text.txt", "w") as my_text:
+        my_text.write("Hello world\n"
+                      "It’s the first exercise in I/O\n"
+                      "That mean it is number 1\n"
+                      "Not 2\n"
+                      "Not three\n"
+                      "It is exciting\n"
+                      "And i am all 4 it")
+except IOError as ex:
+    print("Error performing I/O operations on the file: ",ex)
